7d074a18a49a594f1c18f30e517e74362512697f
Dave Aitken <dave.aitken@gmail.com>
2017-09-17 12:08:12 +0100

    Render grid with SVG and listen for clicks

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,11 @@
 module Main exposing (..)
 
+import Dict exposing (Dict)
 import Html exposing (Html, text, div, img, h1)
+import Html.Events exposing (onClick)
 import Html.CssHelpers
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 import Styles
 
 
@@ -9,26 +13,72 @@ import Styles
 
 
 type alias Coord =
-    { x : Int
-    , y : Int
-    }
+    ( Int, Int )
 
 
 type alias Model =
-    { position : Coord
+    { terrain : Dict Coord Tile
+    , position : Coord
     , path : List Coord
-    , size : Int
+    , svgSize : ( Int, Int )
     }
+
+
+type Tile
+    = E -- empty
+    | Rock
+
+
+terrainGrid : List (List Tile)
+terrainGrid =
+    [ [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    , [ E, E, E, E, E, E, E, E, E, E ]
+    ]
+
+
+terrainCols : Int
+terrainCols =
+    terrainGrid
+        |> List.head
+        |> Maybe.withDefault []
+        |> List.length
+
+
+terrainRows : Int
+terrainRows =
+    terrainGrid
+        |> List.length
+
+
+terrainDict : Dict Coord Tile
+terrainDict =
+    terrainGrid
+        |> List.indexedMap
+            (\y row ->
+                List.indexedMap
+                    (\x tile ->
+                        ( ( x, y ), tile )
+                    )
+                    row
+            )
+        |> List.concat
+        |> Dict.fromList
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { position =
-            { x = 0
-            , y = 0
-            }
+    ( { terrain = terrainDict
+      , position = ( 0, 0 )
       , path = []
-      , size = 20
+      , svgSize = ( 512, 512 )
       }
     , Cmd.none
     )
@@ -39,12 +89,15 @@ init =
 
 
 type Msg
-    = NoOp
+    = GridClick ( Int, Int )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+update (GridClick ( x, y )) model =
+    let
+        dummy = Debug.log "GridClick" ( x, y )
+    in
+        ( model, Cmd.none )
 
 
 
@@ -55,27 +108,104 @@ update msg model =
     Html.CssHelpers.withNamespace Styles.ns
 
 
-grid : Model -> Html Msg
-grid model =
-    div
-        [ class [ Styles.Grid ] ]
-        (List.repeat model.size
-            (div [ class [ Styles.GridRow ] ]
-                (List.repeat model.size
-                    (div [ class [ Styles.GridCell ] ]
-                        [ text "x" ]
-                    )
-                )
-            )
+
+-- divGrid : Model -> Html Msg
+-- divGrid model =
+--     let (w, h) = model.size in
+--     div
+--         [ class [ Styles.Grid ] ]
+--         (List.repeat h
+--             (div [ class [ Styles.GridRow ] ]
+--                 (List.repeat model.size
+--                     (div [ class [ Styles.GridCell ] ]
+--                         [ Html.text "x" ]
+--                     )
+--                 )
+--             )
+--         )
+
+
+calcDeltas : Model -> ( Int, Int )
+calcDeltas model =
+    let
+        ( w, h ) =
+            model.svgSize
+    in
+        ( w // terrainRows
+        , h // terrainCols
         )
+
+
+clickListeners : Model -> List (Svg Msg)
+clickListeners model =
+    let
+        ( dx, dy ) =
+            calcDeltas model
+
+        terrainList =
+            Dict.toList model.terrain
+    in
+        List.map
+            (\( ( x, y ), tile ) ->
+                Svg.rect
+                    [ Svg.Attributes.x (toString (x * dx))
+                    , Svg.Attributes.y (toString (y * dy))
+                    , width (toString dx)
+                    , height (toString dy)
+                    , fill "red"
+                    , fillOpacity "0"
+                    , onClick (GridClick ( x, y ))
+                    ]
+                    []
+            )
+            terrainList
+
+
+cellPoints : Model -> List (Svg Msg)
+cellPoints model =
+    let
+        ( dx, dy ) =
+            calcDeltas model
+
+        terrainList =
+            Dict.toList model.terrain
+    in
+        List.map
+            (\( ( x, y ), tile ) ->
+                Svg.circle
+                    [ Svg.Attributes.cx (toString (x * dx + dx // 2))
+                    , Svg.Attributes.cy (toString (y * dy + dy // 2))
+                    , r "2"
+                    , fill "black"
+                    ]
+                    []
+            )
+            terrainList
+
+
+svgGrid : Model -> Svg Msg
+svgGrid model =
+    let
+        ( w, h ) =
+            model.svgSize
+    in
+        svg
+            [ width (toString w)
+            , height (toString h)
+            ]
+            (List.concat
+                [ (cellPoints model)
+                , (clickListeners model)
+                ]
+            )
 
 
 view : Model -> Html Msg
 view model =
-    div [ id [ Styles.Page ]]
-        [ h1 [] [ (text "Pathfinder") ]
-        , div [ class [ Styles.Container ]]
-            [ grid model
+    div [ id [ Styles.Page ] ]
+        [ h1 [] [ (Html.text "Pathfinder") ]
+        , div [ class [ Styles.Container ] ]
+            [ svgGrid model
             ]
         ]
 
